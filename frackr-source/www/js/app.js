eeb82f8e44f0e38e203247d0475f7a8cf75bdef2
Sugata <sugata.a@gmail.com>
2016-01-09 14:24:28 -0500

    added the factory that handles loading and saving

--- frackr-source/www/js/app.js
+++ frackr-source/www/js/app.js
@@ -5,6 +5,38 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('frackr', ['ionic'])
 
+/**
+  The Projects factory handles saving and loading projects from local storage, and also lets us save and load the last active project index.
+**/
+
+.factory('Projects', function() {
+  return {
+    all: function() {
+      var projectString = window.localStorage['projects'];
+      if(projectString) {
+        return angular.fromJson(projectString);
+      }
+      return [];
+    },
+    save: function(projects) {
+      window.localStorage['projects'] = angular.toJson(projects);
+    },
+    newProject: function(projectTitle) {
+      // Add a new project
+      return {
+        title: projectTitle,
+        tasks: []
+      };
+    },
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveProject']) || 0; 
+    },
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveProject'] == index;
+    }
+  }
+})
+
 .controller('FrackrCtrl',function($scope, $ionicModal) {
   
   // No need for testing data anymore
