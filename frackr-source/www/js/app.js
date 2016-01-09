42439e1ccffae58d9bb806f710636738741e8f99
lev18d <sammy.diamantstein@mail.utoronto.ca>
2016-01-09 14:01:12 -0500

    added JS controller create popup addition of information

--- frackr-source/www/js/app.js
+++ frackr-source/www/js/app.js
@@ -4,17 +4,38 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('frackr', ['ionic'])
-.controller('FrackrCtrl', function($scope) {
-  
-  $scope.tasks = [
-    {title: 'Blank'},
-    {title: 'Blank'},
-    {title: 'Blank'},
-    {title: 'Blank'},
-  ];
 
+.controller('FrackrCtrl',function($scope, $ionicModal) {
+  // No need for testing data anymore
+  $scope.tasks-[];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal= modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title:task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 })
-
+  
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
